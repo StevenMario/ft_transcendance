152ea732a7fa58a705b49c059633165db02145ed
StevenMario <stevenkoumar@gmail.com>
2026-01-03 22:56:58 +0300

    Making navbar

--- Transcendance/src/components/NavBar.tsx
+++ Transcendance/src/components/NavBar.tsx
@@ -3,7 +3,7 @@ import logo from "../assets/logo.svg";
 export default function NavBar() {
     return (
         <>
-            <nav>
+            <nav className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
                 <img src={logo} alt="Logo" />
             </nav>
         </>
